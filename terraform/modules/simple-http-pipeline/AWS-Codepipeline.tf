871b68e04b5f026e199916c3a3856d7e720dde12
Raaed Serag <raaedserag@gmail.com>
2022-11-23 02:10:03 +0200

    fix deployment

--- terraform/modules/simple-http-pipeline/AWS-Codepipeline.tf
+++ terraform/modules/simple-http-pipeline/AWS-Codepipeline.tf
@@ -19,14 +19,14 @@ resource "aws_codepipeline" "default_pipeline" {
       output_artifacts = ["source_output"]
 
       configuration = {
-        RepositoryName = data.aws_codecommit_repository.default_repo.repository_name
-        BranchName     = var.repository_branch
+        RepositoryName       = data.aws_codecommit_repository.default_repo.repository_name
+        BranchName           = var.repository_branch
         PollForSourceChanges = "false"
       }
     }
   }
   stage {
-    name = "Build"
+    name = "Staging"
 
     action {
       name            = "Build"
@@ -34,47 +34,32 @@ resource "aws_codepipeline" "default_pipeline" {
       owner           = "AWS"
       provider        = "CodeBuild"
       input_artifacts = ["source_output"]
-      output_artifacts = ["build_output"]
       version         = "1"
       configuration = {
-        ProjectName = aws_codebuild_project.build_images.name
+        ProjectName = aws_codebuild_project.staging_build.name
       }
     }
-  }
-  stage {
-    name = "Deploy"
-
     action {
-      name            = "Deploy"
-      category        = "Build"
+      name            = "Build"
+      category        = "Test"
       owner           = "AWS"
       provider        = "CodeBuild"
+      input_artifacts = ["source_output"]
       version         = "1"
-      input_artifacts = ["build_output"]
       configuration = {
-        ProjectName = aws_codebuild_project.deploy.name
+        ProjectName = aws_codebuild_project.staging_test.name
       }
     }
+    # action {
+    #   name            = "Build"
+    #   category        = "Build"
+    #   owner           = "AWS"
+    #   provider        = "CodeBuild"
+    #   input_artifacts = ["source_output"]
+    #   version         = "1"
+    #   configuration = {
+    #     ProjectName = aws_codebuild_project.staging_build.name
+    #   }
+    # }
   }
-
-  # stage {
-  #   name = "Deploy"
-
-  #   action {
-  #     name            = "Deploy"
-  #     category        = "Deploy"
-  #     owner           = "AWS"
-  #     provider        = "CloudFormation"
-  #     input_artifacts = ["build_output"]
-  #     version         = "1"
-
-  #     configuration = {
-  #       ActionMode     = "REPLACE_ON_FAILURE"
-  #       Capabilities   = "CAPABILITY_AUTO_EXPAND,CAPABILITY_IAM"
-  #       OutputFileName = "CreateStackOutput.json"
-  #       StackName      = "MyStack"
-  #       TemplatePath   = "build_output::sam-templated.yaml"
-  #     }
-  #   }
-  # }
 }
--- terraform/modules/simple-http-pipeline/staging-phase.tf
+++ terraform/modules/simple-http-pipeline/staging-phase.tf
@@ -8,14 +8,14 @@ locals {
     }
   )
 }
-resource "aws_codebuild_project" "build_images" {
+resource "aws_codebuild_project" "staging_build" {
   name          = "build-${var.staging_environment_config.name}-simple-http"
-  description   = "Builds the Docker images for the simple-http application"
-  build_timeout = "10"
+  description   = "Build staging images for simple-http application"
+  build_timeout = "5"
   service_role  = aws_iam_role.pipeline_role.arn
   source {
     type      = "CODEPIPELINE"
-    buildspec = "devops/build-images.buildspec.yml"
+    buildspec = "devops/build.buildspec.yml"
   }
   artifacts {
     type = "CODEPIPELINE"
@@ -39,19 +39,18 @@ resource "aws_codebuild_project" "build_images" {
   logs_config {
     cloudwatch_logs {
       group_name  = aws_cloudwatch_log_group.pipeline_log_group.name
-      stream_name = "${var.staging_environment_config.name}/build-images"
+      stream_name = "${var.staging_environment_config.name}/build"
     }
   }
 }
-
-resource "aws_codebuild_project" "deploy" {
-  name          = "deploy-${var.staging_environment_config.name}-simple-http"
-  description   = "Deploy the images for the simple-http application"
-  build_timeout = "10"
+resource "aws_codebuild_project" "staging_test" {
+  name          = "testing-${var.staging_environment_config.name}-simple-http"
+  description   = "Testing staging images for simple-http application"
+  build_timeout = "5"
   service_role  = aws_iam_role.pipeline_role.arn
   source {
     type      = "CODEPIPELINE"
-    buildspec = "devops/deploy-images.buildspec.yml"
+    buildspec = "devops/testing.buildspec.yml"
   }
   artifacts {
     type = "CODEPIPELINE"
@@ -62,7 +61,41 @@ resource "aws_codebuild_project" "deploy" {
     image                       = "aws/codebuild/standard:4.0"
     compute_type                = "BUILD_GENERAL1_SMALL"
     image_pull_credentials_type = "CODEBUILD"
-    privileged_mode             = false
+    privileged_mode             = true
+    dynamic "environment_variable" {
+      for_each = local.environment_variables
+      content {
+        name  = environment_variable.key
+        value = environment_variable.value
+      }
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = aws_cloudwatch_log_group.pipeline_log_group.name
+      stream_name = "${var.staging_environment_config.name}/testing"
+    }
+  }
+}
+resource "aws_codebuild_project" "staging_deploy" {
+  name          = "deploy-${var.staging_environment_config.name}-simple-http"
+  description   = "Deploy staging images for simple-http application"
+  build_timeout = "5"
+  service_role  = aws_iam_role.pipeline_role.arn
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "devops/deploy.buildspec.yml"
+  }
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    type                        = "LINUX_CONTAINER"
+    image                       = "aws/codebuild/standard:4.0"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image_pull_credentials_type = "CODEBUILD"
     dynamic "environment_variable" {
       for_each = local.environment_variables
       content {
